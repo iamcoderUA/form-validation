908f81c3b8f2c1ee948bd92fa61e858a4807a094
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-06-01 18:39:46 +0300

    fixed<valid-inputs.js> code refactoring

--- src/js/modules/valid-inputs.js
+++ src/js/modules/valid-inputs.js
@@ -4,7 +4,7 @@ export const errorMessage = {
   network: 'Подключение к интернету нарушено',
   error: 'Произошла ошибка на сервере',
   success: 'Регистрация прошла успешно, проверьте почту',
-}
+};
 export const errorInputText = document.querySelector('.login-form__error-text');
 export const errorPopup = document.querySelector('.login-form__error');
 
@@ -15,19 +15,21 @@ export default function validInputs(input) {
   const regexp = /^([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
 
   const removeClasses = (value) => {
-    value.classList.remove('js-invalid');
-    value.classList.remove('js-checked');
+    value.classList.remove('js-invalid', 'js-checked');
     passwordError.classList.remove('js-active');
     errorPopup.classList.remove('js-active');
   };
-  const addInvalidClasses = (value) => {
+  const addInvalidClasses = (value, errorText) => {
     value.classList.add('js-invalid');
     value.classList.remove('js-checked');
+    errorPopup.classList.add('js-active');
+    errorInputText.textContent = errorText;
     flag = false;
   };
   const addCheckedClass = (value) => {
     value.classList.add('js-checked');
     value.classList.remove('js-invalid');
+    errorPopup.classList.remove('js-active');
     flag = true;
   };
 
@@ -38,27 +40,16 @@ export default function validInputs(input) {
     switch (input.getAttribute('name')) {
       case 'email':
         checkEmail = regexp.test(input.value);
-        if (!checkEmail)  {
-          addInvalidClasses(input);
-          errorInputText.textContent = errorMessage.email;
-          errorPopup.classList.add('js-active');
-        }
-        else {
-          addCheckedClass(input);
-          errorPopup.classList.remove('js-active');
-        }
+        if (!checkEmail) addInvalidClasses(input, errorMessage.email);
+        else addCheckedClass(input);
         break;
       case 'password':
         if (input.value.length < 4) {
-          addInvalidClasses(input);
+          addInvalidClasses(input, errorMessage.password);
           passwordError.classList.add('js-active');
-          errorPopup.classList.add('js-active');
-          errorInputText.textContent = errorMessage.password
-        }
-        else {
+        } else {
           addCheckedClass(input);
           passwordError.classList.remove('js-active');
-          errorPopup.classList.remove('js-active');
         }
         break;
       case 'checkbox':
