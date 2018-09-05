39f380381e541de15942b3bdf750411d29c97c2c
Stanislav Mikhniev <stanislav.mikhniev@gmail.com>
2018-09-05 22:58:44 +0300

    refactor(server.js): changed to tempalte string instead of concatenation

--- server.js
+++ server.js
@@ -11,20 +11,20 @@ const port = process.env.PORT;
 app.set('views', './prod');
 app.set('view engine', 'html');
 
-app.use(express.static(__dirname + '/prod/'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(`${__dirname}/prod/`));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  next();
 });
 app.post('/send-mail', sendMail);
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/prod/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/prod/index.html`));
 });
 app.listen(port, (req, res) => {
-    console.log('Server is running at port: ', port);
+  console.log('Server is running at port: ', port);
 });
